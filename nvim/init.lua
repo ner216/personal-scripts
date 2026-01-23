32d0e0aed2938c8649b0e062c4e7a3d192115008
ner216 <provencher.nolan@gmail.com>
2026-01-23 08:33:26 -0500

    feat: Add neovim configuration folder.

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.syntax = "on"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
