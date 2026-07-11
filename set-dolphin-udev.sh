91ed360545e433aa53799faff790f12d48f90414
ner216 <provencher.nolan@gmail.com>
2026-07-11 09:19:37 -0400

    Update dolphin script.

--- set-dolphin-udev.sh
+++ set-dolphin-udev.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo echo "SUBSYSTEM=="hidraw", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0306", MODE="0666", GROUP="plugdev"" > /etc/udev/rules.d/51-wiimote.rules
+sudo tee /etc/udev/rules.d/51-wiimote.rules > /dev/null << 'EOF'
+# Wii Remote / DolphinBar hidraw access
+SUBSYSTEM=="hidraw", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0306", MODE="0666", GROUP="plugdev"
+
+# Wii Remote / DolphinBar raw USB access (for Bluetooth Passthrough)
+SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0306", MODE="0666", GROUP="plugdev"
+EOF
 
 sudo udevadm control --reload-rules && sudo udevadm trigger
 
